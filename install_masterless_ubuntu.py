cc83a85ecd31cf1c98e798c2bdaede3e32e34ebb
Konstantin Baikov <konstantin.baikov@tribe29.com>
2021-07-05 11:21:16 +0200

    Use main in python script to avoid execution

--- install_masterless_ubuntu.py
+++ install_masterless_ubuntu.py
@@ -7,9 +7,6 @@ MINION_CONFD = Path("/etc/salt/minion.d")
 PILLAR_PATH = Path("/srv/pillar")
 SALT_FILE_ROOT = Path("/srv/salt")
 
-subprocess.run("apt install --yes -q salt-minion".split())
-subprocess.run("systemctl stop salt-minion".split())
-subprocess.run("systemctl disable salt-minion".split())
 
 masterless_contents = """file_client: local
 file_roots:
@@ -18,17 +15,24 @@ file_roots:
 
 """
 
-Path(MINION_CONFD / "masterless.conf").write_text(masterless_contents)
 
-PILLAR_PATH.mkdir(exist_ok=True)
-SALT_FILE_ROOT.mkdir(exist_ok=True)
+def main():
+    subprocess.run("apt install --yes -q salt-minion".split())
+    subprocess.run("systemctl stop salt-minion".split())
+    subprocess.run("systemctl disable salt-minion".split())
 
-subprocess.run(
-    f"git clone https://github.com/kbaikov/salt-states.git {SALT_FILE_ROOT}".split()
-)
+    Path(MINION_CONFD / "masterless.conf").write_text(masterless_contents)
+
+    PILLAR_PATH.mkdir(exist_ok=True)
+    SALT_FILE_ROOT.mkdir(exist_ok=True)
+
+    subprocess.run(f"git clone https://github.com/kbaikov/salt-states.git {SALT_FILE_ROOT}".split())
+
+    # sync execution modules and states
+    subprocess.run("salt-call saltutil.sync_all".split())
+
+    subprocess.run("salt-call state.apply".split())
 
 
-# sync execution modules and states
-subprocess.run("salt-call saltutil.sync_all".split())
-
-subprocess.run("salt-call state.apply".split())
+if __name__ == "__main__":
+    exit(main)
